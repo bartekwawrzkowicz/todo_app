0d99c5801806e18b9d33c8f72fbea3c3947b1b14
Bartosz <73258197+bartekwawrzkowicz@users.noreply.github.com>
2021-10-26 16:37:51 +0200

    starting React Native app

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Zaczynamy programowanie z React Native</Text>
       <StatusBar style="auto" />
     </View>
   );
